5c843d1e0fe3f62cf6a076d9c605f90b70a22266
quanlong <2603302308@qq.com>
2020-03-28 17:05:57 +0800

    pytest demo再次提交

--- test/test_pytest.py
+++ test/test_pytest.py
@@ -9,6 +9,10 @@ from python.calc import Calc
 
 
 class calc:
+    def setup(self):
+        self.calc = Calc()
     def test_add(self):
-        calc = Calc()
-        assert calc.add(1, 2) == 3
+        assert self.calc.add(1, 2) == 3
+
+    def test_div(self):
+        assert self.calc.div(1, 2) == 0.5
