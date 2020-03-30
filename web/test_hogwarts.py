249b8d9f7ccb99802b5adacae1fb69778245ff1e
quanlong <2603302308@qq.com>
2020-03-30 21:55:42 +0800

    selenium搭建环境

--- web/test_hogwarts.py
+++ web/test_hogwarts.py
@@ -6,3 +6,20 @@ created on 2019-6-2
 project:
 '''
 from selenium import webdriver
+from time import sleep
+
+
+class TestHogwarts():
+    def setup(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        # 隐式等待5秒，只能查找元素，不知道能否点击
+        self.driver.implicitly_wait(5)
+
+    def teardown(self):
+        self.driver.quit()
+
+    def test_hogwarts(self):
+        self.driver.get("http://www.testerhome.com")
+        self.driver.find_element_by_link_text("社团").click()
+        self.driver.find_element_by_link_text("霍格沃兹测试学院").click()
